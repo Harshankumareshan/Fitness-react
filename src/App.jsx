22e5f37da89c709be86a0aa8708bb5c3bf965fa9
harshankumareshan <harshank333@gmail.com>
2024-04-07 20:16:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Hero from './components/Hero/Hero'
+import Join from './components/Join/Join'
+import Plans from './components/Plans/Plans'
+import Reasons from './components/Reasons/Reasons'
+import Testimonials from './components/Testimonials/Testimonials'
+import Programs from './components/programs/Programs'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hero/>
+      <Programs/>
+      <Reasons/>
+      <Plans/>
+      <Testimonials/>
+      <Join/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
